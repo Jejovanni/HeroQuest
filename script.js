ed0a4f24ec382b53d046b6afd43732660eee8e27
jejovanni <jamaineekong@gmail.com>
2023-01-22 10:51:43 +0100

    new start btn

--- script.js
+++ script.js
@@ -1,44 +1,75 @@
-(() => {
+(function (){
 
 //when the page loads it should display an input which takes a value 
 //when value is submitted, it should be assigned to a variable
 //as the value is being submitted it should also invoke a function that starts off the game  
-
     var conDiv = document.getElementById('container');
-    const heroName = document.createElement("form");
-    heroName.setAttribute('id', 'hero_name');
+    //const heroName = document.createElement("form");
+   // heroName.setAttribute('id', 'hero_name');
+    
 
-    var heroInput = document.createElement('input');
-    heroInput.setAttribute('placeholder', 'What is your name?');
-    heroInput.setAttribute('type', 'text');
-    heroInput.setAttribute('required', '');
+    //var heroInput = document.createElement('input');
+    //heroInput.setAttribute('placeholder', 'What is your name?');
+    //heroInput.setAttribute('type', 'text');
+    //heroInput.setAttribute('required', '');
 
-    heroName.appendChild(heroInput);
-    conDiv.appendChild(heroName);
+    var startBtn = document.createElement('button');
+    startBtn.setAttribute('type', 'submit');
+    startBtn.innerHTML = 'START GAME';
+    startBtn.setAttribute('id', 'btnStart');
+    
 
-    document.addEventListener('keydown', (event) => {
-        const key = event.code;
+    //heroName.appendChild(heroInput);
+    //heroName.appendChild(heroBtn);
+    //conDiv.appendChild(heroName);
+    conDiv.appendChild(startBtn);
+    //const hero = document.querySelector('input').value;
 
-        if(key === 'KeyEnter' ){
-           const hero = document.querySelector('input').value;
-           document.removeChild(heroName);
-           console.log(hero);
-
+    startBtn.addEventListener('click', function (event){
+        //let key = event.key;
+       event.preventDefault();
+           //heroName.remove();
+           startBtn.remove();
            startGame();
-
-        }
-
+           
     });
 
 })();
- 
+  
     function startGame(){
         //create a function that will fire up the game, first it'll load epilogue, and present a choice
         //this weill be done by creating and appending a div with a paragraph in it, then you change the innerHTML or innerText
-       var questPara = document.createElement('p');
-        var question = document.setAttribute('id', 'quest');
-        questPara.appendChild(question);
+        var conDiv = document.getElementById('container');
+        const heroName = document.createElement("form");
+        heroName.setAttribute('id', 'hero_name');
+        const hero = document.querySelector('input').value;
+
+
+        var heroInput = document.createElement('input');
+        heroInput.setAttribute('placeholder', 'What is your name?');
+        heroInput.setAttribute('type', 'text');
+        heroInput.setAttribute('required', '');
+
+        var heroBtn = document.createElement('button');
+        heroBtn.setAttribute('id', heroBtn);
+        heroBtn.setAttribute('type', 'submit');
+        heroBtn.innerHTML = '->';
+
+        heroName.appendChild(heroInput);
+        heroName.appendChild(heroBtn);
+        conDiv.appendChild(heroName);
+
+        heroBtn.addEventListener('click', (event)=>{
+            event.preventDefault();
+            heroName.remove;
+        })
+
+        
+        var questPara = document.createElement('p');
+        questPara.setAttribute('id', 'quest');
+        questPara.innerHTML = 'Hello there!';
         conDiv.appendChild(questPara);
+        console.log(hero);
 
     }
    
